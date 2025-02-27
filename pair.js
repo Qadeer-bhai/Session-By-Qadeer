5b86194c47245df5f1fb58436391872bab2a5e96
Qadeer-bhai <ummerkulachi@gmail.com>
2025-02-27 13:43:22 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,14 +57,14 @@ router.get('/', async (req, res) => {
 
                let QADEER_KHAN_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃ᴍᴀʟᴠɪɴ-ᴍᴅ SESSION IS 
+┃𝑸𝒂𝒅𝒆𝒆𝒓-𝑲𝒉𝒂𝒏 SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘⚡ ᴍᴀʟᴠɪɴ-ᴋɪɴɢ ⚡𖥘
+❶ || Creator = 𖥘⚡ 𝐐𝐀𝐃𝐄𝐄𝐑-𝐊𝐇𝐀𝐍⚡𖥘
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z
+❷ || https://whatsapp.com/channel/0029Vaw6yRaBPzjZPtVtA80A
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ❸ || Owner = https://wa.me/923079749129
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
@@ -72,16 +72,16 @@ router.get('/', async (req, res) => {
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ❻ || YouTube = https://www.youtube.com/@tofanmods7 
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©2024-2099 ᴍᴀʟᴠɪɴ-ᴋɪɴɢ_`
- await Pair_Code_By_Malvin_King.sendMessage(Pair_Code_By_Malvin_King.user.id,{text:MALVIN_KING_TEXT},{quoted:session})
+©2024-2099 Qadeer-Khan_`
+ await Pair_Code_By_Qadeer_Khan.sendMessage(Pair_Code_By_Qadeer_Khan.user.id,{text:QADEER_KHAN_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Malvin_King.ws.close();
+        await Pair_Code_By_Qadeer_Khan.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    MALVIN_KING_PAIR_CODE();
+                    QADEER_KHAN_PAIR_CODE();
                 }
             });
         } catch (err) {
